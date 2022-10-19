f64cdf8aa8639ef41c2dbf57388389bc720f7cb0
fifaliana HARENA <harenafifaliana.181@gmail.com>
2022-10-19 07:48:48 +0300

    portfolio

---
+++ portfolio/index.js
@@ -0,0 +1,26 @@
+function getContainer() {
+    let header = document.querySelector("header");
+    let contenue = document.querySelector("#body");
+    let footer = document.querySelector("footer");
+    return {
+        header,
+        contenue,
+        footer,
+    };
+}
+
+const { header, contenue, footer } = getContainer();
+
+const loadContent = async () => {};
+
+// Fonction recuperer contenue : parametre (balise ou identifiant : string)
+const loadHTML = (url, div) => {
+    fetch(url)
+        .then((reponse) => reponse.text())
+        .then((contenue) => (document.getElementById(div).innerHTML = contenue));
+};
+
+
+loadHTML("./header/header.html","entete")
+loadHTML("./contenue/contenue.html","body")
+loadHTML("./footer/footer.html","piedPage")
