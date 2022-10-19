0e3dcd143c18e586831afb97839b8f047e4c7e2e
fifaliana HARENA <harenafifaliana.181@gmail.com>
2022-10-19 13:41:54 +0300

    animation titre,cursor

--- portfolio/assets/js/contenue.js
+++ portfolio/assets/js/contenue.js
@@ -148,6 +148,59 @@ sr.reveal(`.category__data, .trick__content, .footer__content`, {
 sr.reveal(`.about__data, .discount__img`, { origin: "left" });
 sr.reveal(`.about__img, .discount__data`, { origin: "right" });
 
-// slide
+// cursor
+let cursor = document.querySelector(".cursor");
+let cursor2 = document.querySelector(".cursor2");
+let cursorScale = document.querySelectorAll(".cursor-scale");
+let mouseX = 0;
+let mouseY = 0;
 
-// fin slide
+gsap.to({}, 0.016, {
+    repeat: -1,
+    onRepeat: function () {
+        gsap.set(cursor, {
+            css: {
+                left: mouseX,
+                top: mouseY,
+            },
+        });
+        gsap.set(cursor2, {
+            css: {
+                left: mouseX,
+                top: mouseY,
+            },
+        });
+    },
+});
+
+window.addEventListener("mousemove", (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+});
+
+cursorScale.forEach((link) => {
+    link.addEventListener("mousemove", () => {
+        cursor.classList.add("grow");
+        if (link.classList.contains("small")) {
+            cursor.classList.remove("grow");
+            cursor.classList.add("grow-small");
+        }
+    });
+
+    link.addEventListener("mouseleave", () => {
+        cursor.classList.remove("grow");
+        cursor.classList.remove("grow-small");
+    });
+});
+
+// cursor
+// letre
+var string =
+  "Hello Welcome to Srki.tk , We hope you find what you're looking for and that you enjoy.";
+var str = string.split("");
+var el = document.getElementById("str");
+(function animate() {
+  str.length > 0 ? (el.innerHTML += str.shift()) : clearTimeout(running);
+  var running = setTimeout(animate, 120);
+})();
+// fin letre
